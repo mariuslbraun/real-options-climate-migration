76cc0dda353ba4f781821ca8e10157aac9349fff
mariuslbraun <marius.braun-2@wirtschaft.uni-giessen.de>
2023-07-11 09:30:55 +0200

    refactored estimation and plotting of GAMs with contiguity and OECD destination interactions

--- scripts/functions.R
+++ scripts/functions.R
@@ -13,8 +13,24 @@ library(tidymv)
 # clear workspace
 rm(list = ls())
 
+# load data frames
+get_df =  function(sample){
+  df = readRDS(
+      paste0(
+        "prepared/",
+        paste(
+          "df",
+          sample,
+          sep = "_"
+        ),
+        ".rds"
+      )
+    )
+  return(df)
+}
+
 # estimate and save GAM
-estimate_GAM = function(sample, formulae, model_type) {
+estimate_GAM = function(sample, formulae, model_type, by) {
   # create data frame name
   df_name = paste(
     "df",
@@ -25,12 +41,31 @@ estimate_GAM = function(sample, formulae, model_type) {
   # get regression formula
   formulae = as.formula(formulae)
   
-  # create model name
-  model_name = paste(
-    model_type,
-    sample,
-    sep = "_"
-  )
+  if(missing(by)) {
+    # create model name
+    model_name = paste(
+      ifelse(
+        missing(model_type),
+        "gam",
+        model_type
+      ),
+      sample,
+      sep = "_"
+    )
+  } else {
+    # create model name
+    model_name = paste(
+      ifelse(
+        missing(model_type),
+        "gam",
+        model_type
+      ),
+      sample,
+      by,
+      sep = "_"
+    )
+  }
+  
   
   # estimate model
   gc()
@@ -41,50 +76,106 @@ estimate_GAM = function(sample, formulae, model_type) {
     method = "REML"
   )
   
-  # save model as RDS file
-  saveRDS(
-    model,
-    paste0(
-      "models/main_results/",
-      model_name,
-      ".rds"
+  if(missing(by)){
+    # save model as RDS file
+    saveRDS(
+      model,
+      paste0(
+        "models/main_results/",
+        model_name,
+        ".rds"
+      )
     )
-  )
-  
-  # return model
-  return(model)
+    
+    # return model
+    return(model)
+  } else {
+    # save model as RDS file
+    saveRDS(
+      model,
+      paste0(
+        "models/",
+        by,
+        "/",
+        model_name,
+        ".rds"
+      )
+    )
+    
+    # return model
+    return(model)
+  }
 }
 
 # plot smooth and save as PDF
-show_gam = function(sample, model_type, climate_var, model) {
-  plot_name = paste(
-    model_type,
-    climate_var,
-    sample,
-    sep = "_"
-  )
-  
-  pdf(
-    paste0(
-      "figures/main_results/",
-      plot_name,
-      ".pdf"
+show_gam = function(sample, climate_var, model, by) {
+  if(missing(by)) {
+    plot_name = paste(
+      "gam",
+      climate_var,
+      sample,
+      sep = "_"
     )
-  )
-  plot_smooth(
-    x = model,
-    view = climate_var,
-    xlab = ifelse(
-      climate_var == "temp_anom",
-      "Temperature anomalies",
-      "Precipitation anomalies"
-    ),
-    ylab = "log of bilateral migration rates",
-    h0 = 0,
-    v0 = 0,
-    rm.ranef = T
-  )
-  dev.off()
+    
+    pdf(
+      paste0(
+        "figures/main_results/",
+        plot_name,
+        ".pdf"
+      )
+    )
+    
+    plot_smooth(
+      x = model,
+      view = climate_var,
+      xlab = ifelse(
+        climate_var == "temp_anom",
+        "Temperature anomalies",
+        "Precipitation anomalies"
+      ),
+      ylab = "log of bilateral migration rates",
+      h0 = 0,
+      v0 = 0,
+      rm.ranef = T
+    )
+    
+    dev.off()
+  } else {
+    plot_name = paste(
+      "gam",
+      climate_var,
+      sample,
+      by,
+      sep = "_"
+    )
+    
+    pdf(
+      paste0(
+        "figures/",
+        by,
+        "/",
+        plot_name,
+        ".pdf"
+      )
+    )
+    
+    plot_smooth(
+      x = model,
+      view = climate_var,
+      plot_all = by,
+      xlab = ifelse(
+        climate_var == "temp_anom",
+        "Temperature anomalies",
+        "Precipitation anomalies"
+      ),
+      ylab = "log of bilateral migration rates",
+      h0 = 0,
+      v0 = 0,
+      rm.ranef = T
+    )
+    
+    dev.off()
+  }
 }
 
 compare_GAM_GLM = function(sample) {
--- scripts/results.R
+++ scripts/results.R
@@ -26,23 +26,16 @@ source("scripts/functions.R")
 # load data frames
 samples = c("total", "lowinc", "midinc")
 for(i in 1:length(samples)) {
-  df_name = paste(
-    "df",
-    samples[i],
-    sep = "_"
-  )
   assign(
-    x = df_name,
-    value = readRDS(
-      paste0(
-        "prepared/",
-        df_name,
-        ".rds"
-      )
-    )
+    x = paste(
+      "df",
+      samples[i],
+      sep = "_"
+      ),
+    value = get_df(samples[i])
   )
 }
-rm(i, df_name)
+rm(i)
 
 
 
@@ -84,9 +77,8 @@ for(i in 1:length(samples)) {
     # plot smooths
     for(k in 1:length(climate_vars)) {
       if(model_types[j] == "gam") {
-        show_gam(
+        plot = show_gam(
           sample = samples[i],
-          model_type = model_types[j],
           climate_var = climate_vars[k],
           model = get(model_name)
         )
@@ -98,86 +90,51 @@ for(i in 1:length(samples)) {
   compare_GAM_GLM(samples[i])
 }
 toc()
-rm(i, j, k, model_name)
+rm(i, j, k, model_name, plot)
 
 
 
-#### 2.2 Additional results 1: interaction terms for contiguity ####
-
-# formula for regression models
-contig_formula_gam = "mig_rate_new ~ contiguity + s(temp_anom, bs='cr')  + s(temp_anom,by = contiguity, bs='cr') + s(precip_anom, bs='cr')+ s(precip_anom, by = contiguity, bs='cr') + period + X...origin + destination"
-
-# GAM for low-income countries
-gc()
-tic()
-gam_lowinc_contig = mgcv::gam(contig_formula_gam, family = Gamma(link = "log"),
-                            data = df_lowinc, method = "REML")
-toc()
-summary(gam_lowinc_contig)
-
-# plot smooth nonparametric functions of temperature and precipitation anomalies
-plot(gam_lowinc_contig, shade = TRUE, xlab = "Temperature anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-plot(gam_lowinc_contig, shade = TRUE, xlab = "Precipitation anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-abline(h=0, v=0, lty=2)
-gam.check(gam_lowinc_contig)
-
-# GAM for middle-income countries
-gc()
-tic()
-gam_midinc_contig = mgcv::gam(contig_formula_gam, family = Gamma(link="log"),
-                            data = df_midinc, method = "REML")
-toc()
-summary(gam_midinc_contig)
-
-# plot smooth nonparametric functions of temperature and precipitation anomalies
-plot(gam_midinc_contig, shade = TRUE, xlab = "Temperature anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-plot(gam_midinc_contig, shade = TRUE, xlab = "Precipitation anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-abline(h=0, v=0, lty=2)
-gam.check(gam_midinc_contig)
-
-
-
-#### 2.3 Additional results 2: interaction terms for OECD destination ####
+#### 2.2 Additional results 1: interaction terms for contiguity and OECD destinations ####
 
 # formula for regression models
-OECD_formula_gam = "mig_rate_new ~ OECD_dest + s(temp_anom, bs='cr') + s(temp_anom, by = OECD_dest, bs='cr') + s(precip_anom, bs='cr') + s(precip_anom, by = OECD_dest, bs='cr') + period + X...origin + destination"
+contiguity_formula_gam = "mig_rate_new ~ contiguity + s(temp_anom, bs='cr')  + s(temp_anom,by = contiguity, bs='cr') + s(precip_anom, bs='cr')+ s(precip_anom, by = contiguity, bs='cr') + period + X...origin + destination"
+OECD_dest_formula_gam = "mig_rate_new ~ OECD_dest + s(temp_anom, bs='cr') + s(temp_anom, by = OECD_dest, bs='cr') + s(precip_anom, bs='cr') + s(precip_anom, by = OECD_dest, bs='cr') + period + X...origin + destination"
 
-# GAM for low-income countries
-gc()
+# vector of interaction names
+interactions = c("contiguity", "OECD_dest")
 tic()
-gam_lowinc_OECD = mgcv::gam(OECD_formula_gam, family = Gamma(link = "log"),
-                            data = df_lowinc, method = "REML")
+for(i in 2:length(samples)) {
+  for(j in 1:length(interactions)) {
+    # model name
+    model_name = paste(
+      "gam",
+      samples[i],
+      interactions[j],
+      sep = "_"
+    )
+    
+    # estimate GAM
+    assign(
+      x = model_name,
+      value = estimate_GAM(
+        sample = samples[i],
+        formulae = get(
+          paste(interactions[j], "formula_gam", sep = "_")
+          ),
+        by = interactions[j]
+      )
+    )
+    
+    # plot smooths
+    for(k in 1:length(climate_vars)) {
+      plot = show_gam(
+        sample = samples[i],
+        climate_var = climate_vars[k],
+        model = get(model_name),
+        by = interactions[j]
+      )
+    }
+  }
+}
 toc()
-summary(gam_lowinc_OECD)
-
-# plot smooth nonparametric functions of temperature and precipitation anomalies
-plot(gam_lowinc_OECD, shade = TRUE, xlab = "Temperature anomalies", ylab = "", cex.lab = 1.4, cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-plot(gam_lowinc_OECD, shade = TRUE, xlab = "Precipitation anomalies", ylab = "", cex.lab = 1.4, cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-abline(h=0, v=0, lty=2)
-gam.check(gam_lowinc_OECD)
-
-# GAM for middle-income countries
-gc()
-tic()
-gam_midinc_OECD = mgcv::gam(OECD_formula_gam, family = Gamma(link = "log"),
-                            data = df_midinc, method = "REML")
-toc()
-summary(gam_midinc_OECD)
-
-# plot smooth nonparametric functions of temperature and precipitation anomalies
-plot(gam_midinc_OECD, shade = TRUE, xlab = "Temperature anomalies", ylab = "", cex.lab = 1.4, cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-plot(gam_midinc_OECD, shade = TRUE, xlab = "Precipitation anomalies", ylab = "", cex.lab = 1.4, cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-abline(h=0, v=0, lty=2)
-gam.check(gam_midinc_OECD)
+rm(i, j, k, model_name, plot)
