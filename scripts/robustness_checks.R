457c8049e67a04150773058ca812f9123edba819
mariuslbraun <marius.braun-2@wirtschaft.uni-giessen.de>
2023-07-31 14:43:46 +0200

    refactored robustness checks: agricultural productivity interaction

--- scripts/robustness_checks.R
+++ scripts/robustness_checks.R
@@ -90,9 +90,7 @@ rm(i, j, k, sample_name, lowinc_dummy_name, model_name, plot)
 
 
 #### 3.2: smoothing parameter selection using GCV instead of REML ####
-
 # estimate GAMs for low-income and middle-income sample
-tic()
 for(i in 2:length(samples)) {
   # model name
   model_name = paste("gam", samples[i], "gcv", sep = "_")
@@ -118,7 +116,6 @@ for(i in 2:length(samples)) {
     )
   }
 }
-toc()
 rm(i, k, model_name, plot)
 
 
@@ -127,7 +124,6 @@ rm(i, k, model_name, plot)
 # formula for regression models
 controls = "log_gdp_pc_ratio_dest_origin + common_lang + log_dist + civil_war"
 controls_formula_gam = paste(baseline_formula_gam, controls, sep = " + ")
-tic()
 for(i in 2:length(samples)) {
   # create log GDP per capita and log distance variables
   df_name = paste("df", samples[i], sep = "_")
@@ -164,19 +160,16 @@ for(i in 2:length(samples)) {
     )
   }
 }
-toc()
 rm(i, k, model_name, plot, controls_formula_gam,
    controls, df_value, df_name)
 
 
 
 #### 3.4: estimate GAM using heat and drought month shares ####
-
 # formula for regression models 
 shares_formula_gam = "mig_rate_new ~ s(share_temp_greater_1SD, bs='cr') + s(share_precip_less_1SD, bs='cr') + period + X...origin + destination"
 climate_shares = c("share_temp_greater_1SD", "share_precip_less_1SD")
 # estimate GAMs for low-income and middle-income sample
-tic()
 for(i in 2:length(samples)) {
     # model name
     model_name = paste("gam", samples[i], "shares", sep = "_")
@@ -201,15 +194,12 @@ for(i in 2:length(samples)) {
       )
     }
 }
-toc()
 rm(i, k, model_name, plot, shares_formula_gam, climate_shares)
 
 
 
 #### 3.5: interaction between climatic anomalies and share of agricultural productivity ####
-
 # create indicator of quartile in the distribution of share of agricultural GDP
-df_total$agri_share_gdp_quantile = NA
 agri_share_quantiles = quantile(df_lowinc$agri_share_gdp, na.rm = T)
 df_total$agri_share_gdp_quantile = ifelse(df_total$agri_share_gdp <= agri_share_quantiles["25%"], "1",
                                    ifelse(df_total$agri_share_gdp > agri_share_quantiles["25%"] &
@@ -218,99 +208,49 @@ df_total$agri_share_gdp_quantile = ifelse(df_total$agri_share_gdp <= agri_share_
                                    df_total$agri_share_gdp <= agri_share_quantiles["75%"], "3", "4")))
 df_total$agri_share_gdp_quantile = as.factor(df_total$agri_share_gdp_quantile)
 
+# estimate models separately for the different quartiles
+for(i in 1:(length(agri_share_quantiles) - 1)) {
+  # create data frame
+  df = df_total %>% filter(agri_share_gdp_quantile == i)
+  sample_name = paste0("agri", i)
+  assign(paste("df", sample_name, sep = "_"), df)
+  
+  # model name
+  model_name = paste("gam", sample_name, sep = "_")
+  
+  # estimate GAM
+  assign(
+    x = model_name,
+    value = estimate_GAM(
+      sample = sample_name,
+      formulae = baseline_formula_gam,
+      directory = "agri"
+    )
+  )
+  
+  # plot smooths
+  for(k in 1:length(climate_vars)) {
+    plot = show_gam(
+      sample = sample_name,
+      climate_var = climate_vars[k],
+      model = get(model_name),
+      directory = "agri"
+    )
+  }
+}
+rm(i, k, n, df, df_name, agri_share_quantiles)
+
 # GAM with interaction between climatic anomalies and factor variable indicating 
 # quartile of agricultural GDP
+agri_formula = "mig_rate_new ~ s(temp_anom, by = agri_share_gdp_quantile, bs = 'cr') + s(precip_anom, by = agri_share_gdp_quantile, bs = 'cr') + period + X...origin + destination"
 gc()
-tic()
-gam_total_agri = mgcv::gam(mig_rate_new ~ s(temp_anom, by = agri_share_gdp_quantile, bs = 'cr') +
-                           s(precip_anom, by = agri_share_gdp_quantile, bs = 'cr') + period + X...origin +
-                           destination, family = Gamma(link="log"), data = df_total, method = "REML")
-toc()
-summary(gam_total_agri)
-
-# create separate dataframes for the quartiles
-n = length(unique(df_total$agri_share_gdp_quantile))-sum(is.na(unique(df_total$agri_share_gdp_quantile)))
-for(i in 1:n) {
-  df = df_total %>% filter(agri_share_gdp_quantile == i)
-  assign(paste0("df_agri", i), df)
-  rm(df)
-}
-rm(i, n, agri_share_quantiles)
-
-# separate GAMs for all four quantiles for plotting (estimated GAMs are equivalent)
-# quantile 1
-gc()
-tic()
-gam_agri1 = mgcv::gam(baseline_formula_gam, family = Gamma(link="log"),
-                            data = df_agri1, method = "REML")
-toc()
-summary(gam_agri1)
-
-# plot smooth nonparametric functions of temperature and precipitation anomalies
-plot(gam_agri1, shade = TRUE, xlab = "Temperature anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-plot(gam_agri1, shade = TRUE, xlab = "Precipitation anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-abline(h=0, v=0, lty=2)
-gam.check(gam_agri1)
-
-# quantile 2
-gc()
-tic()
-gam_agri2 = mgcv::gam(baseline_formula_gam, family = Gamma(link="log"),
-                            data = df_agri2, method = "REML")
-toc()
-summary(gam_agri2)
-
-# plot smooth nonparametric functions of temperature and precipitation anomalies
-plot(gam_agri2, shade = TRUE, xlab = "Temperature anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-plot(gam_agri2, shade = TRUE, xlab = "Precipitation anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-abline(h=0, v=0, lty=2)
-gam.check(gam_agri2)
-
-# quantile 2
-gc()
-tic()
-gam_agri3 = mgcv::gam(baseline_formula_gam, family = Gamma(link="log"),
-                            data = df_agri3, method = "REML")
-toc()
-summary(gam_agri3)
-
-# plot smooth nonparametric functions of temperature and precipitation anomalies
-plot(gam_agri3, shade = TRUE, xlab = "Temperature anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-plot(gam_agri3, shade = TRUE, xlab = "Precipitation anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-abline(h=0, v=0, lty=2)
-gam.check(gam_agri3)
-
-# quantile 4
-gc()
-tic()
-gam_agri4 = mgcv::gam(baseline_formula_gam, family = Gamma(link="log"),
-                            data = df_agri4, method = "REML")
-toc()
-summary(gam_agri4)
-
-# plot smooth nonparametric functions of temperature and precipitation anomalies
-plot(gam_agri4, shade = TRUE, xlab = "Temperature anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-plot(gam_agri4, shade = TRUE, xlab = "Precipitation anomalies", ylab = "", cex.lab = 1.4,
-       cex.axis = 1.3)
-title(ylab = "log of bilateral migration rates", mgp=c(2.5,1,0), cex.lab = 1.4)
-abline(h=0, v=0, lty=2)
-gam.check(gam_agri4)
-
-rm(df_agri1, df_agri2, df_agri3, df_agri4)
+gam_total_agri = mgcv::gam(
+  formula = as.formula(agri_formula),
+  family = Gamma(link="log"),
+  data = df_total,
+  method = "REML"
+)
+saveRDS(gam_total_agri, "models/agri/gam_total_agri.rds")
 
 
 
